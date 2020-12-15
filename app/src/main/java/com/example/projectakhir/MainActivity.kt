71f3fd66b6db309883542a8ef1a503d6fdd1d260
kresnahm <krez2406@gmail.com>
2020-12-15 22:41:32 +0700

    tes awal

--- app/src/main/java/com/example/projectakhir/MainActivity.kt
+++ app/src/main/java/com/example/projectakhir/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.projectakhir
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+    val splashTimeOut = 4000
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         FirebaseApp.initializeApp(this)
+        Handler().postDelayed({
+            val LoginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(LoginIntent)
+            finish()
+        }, splashTimeOut.toLong())
     }
 }
